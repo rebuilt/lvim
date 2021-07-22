eb9dbe278ab7b3b42da5f5507a6fed04b3daff22
rebuilt <memoryman51@hotmail.com>
2021-07-22 11:39:10 +0200

    removed unneeded code, added lazygit, minimap, telescopefzy, markdown-preview, glow, bqf.  Made nvimtree not open by default

--- lv-config.lua
+++ lv-config.lua
@@ -1,36 +1,48 @@
-O.format_on_save = false
+O.format_on_save = true
 O.lint_on_save = true
 O.completion.autocomplete = true
 O.colorscheme = "spacegray"
 O.default_options.wrap = true
 O.default_options.timeoutlen = 500
+O.default_options.relativenumber = true
+vim.cmd(":set hlsearch")
+
+-- ========================================
 -- keymappings
 O.keys.leader_key = "space"
--- overwrite the key-mappings provided by LunarVim for any mode, or leave it empty to keep them
--- O.keys.normal_mode = {
---   Page down/up
---   {'[d', '<PageUp>'},
---   {']d', '<PageDown>'},
---
---   Navigate buffers
---   {'<Tab>', ':bnext<CR>'},
---   {'<S-Tab>', ':bprevious<CR>'},
--- }
--- if you just want to augment the existing ones then use the utility function
--- require("lv-utils").add_keymap_insert_mode({ silent = true }, {
--- { "<C-s>", ":w<cr>" },
--- { "<C-c>", "<ESC>" },
--- })
--- you can also use the native vim way directly
--- vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
+O.lsp.default_keybinds = nil
 
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+vim.cmd("nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>")
+vim.cmd("nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>")
+vim.cmd("nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>")
+vim.cmd("nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>")
+vim.api.nvim_set_keymap(
+	"n",
+	"gl",
+	'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ show_header = false, border = "single" })<CR>',
+	{ noremap = true, silent = true }
+)
+
+vim.cmd("nnoremap <silent> gx <cmd>lua require'lsp'.PeekDefinition()<CR>")
+vim.cmd("map <silent> gy :lua vim.lsp.buf.hover()<CR>")
+vim.cmd("nnoremap <silent> [d :lua vim.lsp.diagnostic.goto_prev({popup_opts = {border = O.lsp.popup_border}})<CR>")
+vim.cmd("nnoremap <silent> ]d :lua vim.lsp.diagnostic.goto_next({popup_opts = {border = O.lsp.popup_border}})<CR>")
+-- scroll down hover doc or scroll in definition preview
+-- scroll up hover doc
+vim.cmd('command! -nargs=0 LspVirtualTextToggle lua require("lsp/virtual_text").toggle()')
+
+vim.api.nvim_set_keymap("n", "<F12>", ":set relativenumber!<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<ESC>", ":nohls | :setlocal nospell<ESC>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gb", "<cmd>BufferPick<CR>", { noremap = true, silent = true })
+
+-- end keymappings
+-- =========================================
 
 O.plugin.dashboard.active = false
 O.plugin.terminal.active = true
 O.plugin.zen.active = false
 O.plugin.zen.window.height = 0.90
+O.plugin.nvimtree.auto_open = 0
 O.plugin.nvimtree.side = "left"
 O.plugin.nvimtree.show_icons.git = 0
 
@@ -39,7 +51,10 @@ O.treesitter.ensure_installed = "maintained"
 O.treesitter.highlight.enabled = true
 
 -- javascript
-O.lang.tsserver.linter = nil
+-- O.lang.tsserver.linter = nil
+O.lang.javascript.formatter.exe = "prettier"
+O.lang.javascript.formatter.args = "--write"
+O.lang.javascript.formatter.stdin = false
 
 O.user_plugins = {
 
@@ -92,7 +107,29 @@ O.user_plugins = {
 	},
 	{
 		"kevinhwang91/nvim-bqf",
-		event = "QuickFixCmdPre",
+		event = { "BufRead", "BufNew" },
+		config = function()
+			require("bqf").setup({
+				auto_enable = true,
+				preview = {
+					win_height = 12,
+					win_vheight = 12,
+					delay_syntax = 80,
+					border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+				},
+				func_map = {
+					vsplit = "",
+					ptogglemode = "z,",
+					stoggleup = "",
+				},
+				filter = {
+					fzf = {
+						action_for = { ["ctrl-s"] = "split" },
+						extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+					},
+				},
+			})
+		end,
 	},
 	{
 		"tpope/vim-bundler",
@@ -104,33 +141,34 @@ O.user_plugins = {
 		ft = { "markdown" },
 		-- run = "yay -S glow"
 	},
-    {
-        "iamcco/markdown-preview.nvim",
-        ft = "markdown",
-        run = ":call mkdp#util#install()",
-        config = function()
-            vim.g.mkdp_auto_start = 1
-        end
-    },
-{
-  "nvim-telescope/telescope-fzy-native.nvim",
-  run = "make",
-},
-	{ 'wfxr/minimap.vim',
-  run = "cargo install --locked code-minimap",
-  cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
-config = function ()
-vim.cmd ("let g:minimap_width = 10")
-vim.cmd ("let g:minimap_auto_start = 0")
-vim.cmd ("let g:minimap_auto_start_win_enter = 0")
-end
-  },
+	{
+		"iamcco/markdown-preview.nvim",
+		ft = "markdown",
+		run = ":call mkdp#util#install()",
+		config = function()
+			vim.g.mkdp_auto_start = 1
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzy-native.nvim",
+		run = "make",
+	},
+	{
+		"wfxr/minimap.vim",
+		run = "cargo install --locked code-minimap",
+		cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+		config = function()
+			vim.cmd("let g:minimap_width = 10")
+			vim.cmd("let g:minimap_auto_start = 0")
+			vim.cmd("let g:minimap_auto_start_win_enter = 0")
+		end,
+	},
 
-{
-  'kdheepak/lazygit.nvim',
-  cmd = {"LazyGit"},
-  requires = { 'nvim-lua/plenary.nvim' },
-}
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = { "LazyGit" },
+		requires = { "nvim-lua/plenary.nvim" },
+	},
 }
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- O.user_autocommands = {{ "BufWinEnter", "*", "echo \"hi again\""}}
@@ -145,7 +183,7 @@ end
 -- }
 
 O.plugin.telescope.on_config_done = function(module)
-  module.load_extension('fzy_native')
+	module.load_extension("fzy_native")
 end
 
 O.plugin.autopairs.on_config_done = function(module)
@@ -173,4 +211,8 @@ O.plugin.which_key.on_config_done = function(module)
 		mode = "v",
 		prefix = "<leader>",
 	})
+end
+
+O.lsp.on_attach_callback = function(client, bufnr)
+	require("lsp_signature").on_attach()
 end
