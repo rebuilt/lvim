fb056b3d68732f7cf72b98c069d404d9071a4fa3
rebuilt <memoryman51@hotmail.com>
2021-08-16 23:33:19 +0200

    add config for emmet

---
+++ lua/user/lsp.lua
@@ -0,0 +1,42 @@
+local M = {}
+
+M.config = function()
+	local lspconfig = require("lspconfig")
+	local configs = require("lspconfig/configs")
+
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities.textDocument.completion.completionItem.snippetSupport = true
+	capabilities.textDocument.completion.completionItem.resolveSupport = {
+		properties = {
+			"documentation",
+			"detail",
+			"additionalTextEdits",
+		},
+	}
+
+	if not lspconfig.emmet_ls then
+		configs.emmet_ls = {
+			default_config = {
+				cmd = { "emmet-ls", "--stdio" },
+				filetypes = {
+					"html",
+					"css",
+					"javascript",
+					"eruby",
+					"typescriptreact",
+					"javascriptreact",
+					"svelte",
+					"vue",
+					"markdown",
+				},
+				root_dir = function(fname)
+					return vim.loop.cwd()
+				end,
+				settings = {},
+			},
+		}
+	end
+	lspconfig.emmet_ls.setup({ capabilities = capabilities })
+end
+
+return M
