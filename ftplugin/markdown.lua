bad86f681787265274f4ea5c49f0639d1046c8bf
rebuilt <memoryman51@hotmail.com>
2021-08-16 23:34:09 +0200

    remove non-working markdown config

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,13 +2,13 @@
 -- npm install --save-dev --save-exact prettier
 -- echo {}> .prettierrc.json
 
-lvim.lang["markdown"] = {
-	formatters = {
-		{
-			exe = "prettier",
-		},
-	},
-}
+-- lvim.lang["markdown"] = {
+-- 	formatters = {
+-- 		{
+-- 			exe = "prettier",
+-- 		},
+-- 	},
+-- }
 
 -- https://github.com/DavidAnson/markdownlint
 -- npm install markdownlint --save-dev
@@ -16,16 +16,15 @@ lvim.lang["markdown"] = {
 -- https://github.com/igorshubovych/markdownlint-cli
 -- npm install -g markdownlint-cli
 -- brew install markdownlint-cli
--- lvim.lang.markdown.linters = {
--- 	{
--- 		-- @usage can be write_good or vale or markdownlint
--- 		exe = "markdownlint",
--- 		args = {},
--- 	},
--- }
--- {
--- 		-- @usage can be write_good or vale or markdownlint
--- 		exe = "vale",
--- 		args = {},
+-- lvim.lang["markdown"] = {
+-- 	linters = {
+-- 		{
+-- 			-- @usage can be write_good or vale or markdownlint
+-- 			exe = "markdownlint",
+-- 		},
+-- 		{
+-- 			-- @usage can be write_good or vale or markdownlint
+-- 			exe = "vale",
+-- 		},
 -- 	},
 -- }
