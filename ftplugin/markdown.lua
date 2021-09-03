9efa52755d2400c8e81694337dbbe29195a97354
rebuilt <memoryman51@hotmail.com>
2021-09-03 12:09:21 +0200

    add markdownlint and writegood

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,30 +1,5 @@
--- https://prettier.io/docs/en/install.html
--- npm install --save-dev --save-exact prettier
--- echo {}> .prettierrc.json
-
--- lvim.lang["markdown"] = {
--- 	formatters = {
--- 		{
--- 			exe = "prettier",
--- 		},
--- 	},
--- }
-
--- https://github.com/DavidAnson/markdownlint
--- npm install markdownlint --save-dev
-
--- https://github.com/igorshubovych/markdownlint-cli
--- npm install -g markdownlint-cli
--- brew install markdownlint-cli
--- lvim.lang["markdown"] = {
--- 	linters = {
--- 		{
--- 			-- @usage can be write_good or vale or markdownlint
--- 			exe = "markdownlint",
--- 		},
--- 		{
--- 			-- @usage can be write_good or vale or markdownlint
--- 			exe = "vale",
--- 		},
--- 	},
--- }
+lvim.lang.markdown = {
+	linters = { { exe = "write_good" }, { exe = "markdownlint" } },
+	formatters = { { exe = "prettier" } },
+}
+require("lsp.null-ls").setup("markdown")
