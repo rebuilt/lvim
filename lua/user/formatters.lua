5ceb45fa0f1a77d5f0e6edcf0e6ae2febb9fb53b
rebuilt <memoryman51@hotmail.com>
2022-07-15 13:29:58 -0700

    Formatter stopped working.  Ignore the lvim table and set a formatter directly with null-ls

--- lua/user/formatters.lua
+++ lua/user/formatters.lua
@@ -1,40 +1,42 @@
 local M = {}
 
 M.setup = function()
-	local formatters = require("lvim.lsp.null-ls.formatters")
-	formatters.setup({
-		{ exe = "black" },
-		{ exe = "stylua" },
-		{ exe = "prettierd" },
-		{ exe = "markdownlint" },
-		{ exe = "codespell" },
-		{ exe = "shfmt" },
-		{ exe = "rustfmt" },
-		{ exe = "rubocop" },
-		{ exe = "pg_format" },
-	})
+	-- local formatters = require("lvim.lsp.null-ls.formatters")
+	-- formatters.setup({
+	-- 	{ exe = "black" },
+	-- 	{ exe = "stylua" },
+	-- 	{ exe = "prettierd" },
+	-- 	{ exe = "markdownlint" },
+	-- 	{ exe = "codespell" },
+	-- 	{ exe = "shfmt" },
+	-- 	{ exe = "rustfmt" },
+	-- 	{ exe = "rubocop" },
+	-- 	{ exe = "erb_lint" },
+	-- 	{ exe = "pg_format" },
+	-- })
+
+	local null_ls = require("null-ls")
+
+	-- lvim.format_on_save = true
+
+	local sources = {
+
+		-- 	-- Formatters
+		-- 	-- null_ls.builtins.formatting.eslint_d,
+		-- 	-- null_ls.builtins.formatting.fixjson,
+		-- 	-- null_ls.builtins.formatting.mix,
+    null_ls.builtins.formatting.stylua,
+
+		-- 	-- Linters
+		-- 	-- null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.diagnostics.rubocop,
+		-- 	-- null_ls.builtins.diagnostics.shellcheck,
+		-- 	-- null_ls.builtins.diagnostics.markdownlint,
+		-- 	-- null_ls.builtins.diagnostics.write_good,
+	}
+
+	-- -- Enable the formatters and linters.
+	null_ls.register({ sources = sources })
 end
 
 return M
--- local null_ls = require("null-ls")
-
--- lvim.format_on_save = true
-
--- local sources = {
-
--- 	-- Formatters
--- 	-- null_ls.builtins.formatting.eslint_d,
--- 	-- null_ls.builtins.formatting.fixjson,
--- 	-- null_ls.builtins.formatting.mix,
--- 	-- null_ls.builtins.formatting.stylua,
-
--- 	-- Linters
--- 	-- null_ls.builtins.diagnostics.eslint_d,
--- 	null_ls.builtins.diagnostics.rubocop,
--- 	-- null_ls.builtins.diagnostics.shellcheck,
--- 	-- null_ls.builtins.diagnostics.markdownlint,
--- 	-- null_ls.builtins.diagnostics.write_good,
--- }
-
--- -- Enable the formatters and linters.
--- null_ls.register({ sources = sources })
--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,36 +1,17 @@
 local M = {}
 
 M.config = function()
+	vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "emmet_ls" })
 	local lspconfig = require("lspconfig")
 	local configs = require("lspconfig/configs")
-
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	capabilities.textDocument.completion.completionItem.snippetSupport = true
-	capabilities.textDocument.completion.completionItem.resolveSupport = {
-		properties = {
-			"documentation",
-			"detail",
-			"additionalTextEdits",
-		},
-	}
 
-	if not lspconfig.emmet_ls then
-		configs.emmet_ls = {
-			default_config = {
-				cmd = { "emmet-ls", "--stdio" },
-				filetypes = {
-					"html",
-					"css",
-					"javascript",
-				},
-				root_dir = function(fname)
-					return vim.loop.cwd()
-				end,
-				settings = {},
-			},
-		}
-	end
-	lspconfig.emmet_ls.setup({ capabilities = capabilities })
+	lspconfig.emmet_ls.setup({
+		-- on_attach = on_attach,
+		capabilities = capabilities,
+		filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "eruby" },
+	})
 end
 
 return M
