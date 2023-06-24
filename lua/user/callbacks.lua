e4bbacf2aabcb8c599474cff932cfc58b97d1ae1
rebuilt <memoryman51@hotmail.com>
2023-06-23 20:47:24 -0700

    Add crystal endwise rules

--- lua/user/callbacks.lua
+++ lua/user/callbacks.lua
@@ -18,18 +18,22 @@ M.setup = function()
     module.add_rule(Rule("<label ", "htmlFor=''></label>", "javascript"))
     module.add_rule(Rule("<input ", "type='text' />", "javascript"))
 
-    -- local endwise = require("nvim-autopairs.ts-rule").endwise
-    -- module.add_rules(endwise(" do$", "end", "elixir", nil))
 
     local endwise = require("nvim-autopairs.ts-rule").endwise
-    -- module.add_rules(require("nvim-autopairs.rules.endwise-lua"))
-
     module.add_rules(
       {
         endwise(" do$", "end", "elixir", nil),
         endwise(" fn ", "end", "elixir", nil),
-        endwise(" def ", "end", "elixir", nil),
-        endwise(" defp ", "end", "elixir", nil)
+        endwise("^%s*def ", "end", "elixir", nil),
+        endwise(" defp ", "end", "elixir", nil),
+      }
+    )
+    module.add_rules(
+      {
+        endwise(" do$", "end", "crystal", nil),
+        endwise("^%s*def ", "end", "crystal", nil),
+        endwise("^class ", "end", "crystal", nil),
+        endwise("|%w*|$", "end", "crystal", nil),
       }
     )
   end
